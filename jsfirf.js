49473586fe529d0f8686f0631cf434250b5de58b
Chris Schalick <cschalick@yahoo.com>
2016-06-12 11:31:27 -0400

    first release

---
+++ jsfirf.js
@@ -0,0 +1,209 @@
+// License information: MIT License
+//
+// Copyright (c) Chris Schalick 2016 All Rights Reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Tree structure code creates an object hierarchy on demand
+// inside an HTML table, allows click to expand / collapse
+// to view and traverse the hierarchy.
+
+function jsfirf(smp, trans, order, win){
+  "use strict";
+  this.smp_freq = smp;
+  this.trans_freq = trans;
+  this.order = order;
+  if((this.order % 2) == 1)
+    this.order++;
+  if(typeof win == "undefined")
+    this.win_type = "rectangular";
+  else
+    this.win_type = win;
+  this.frq_ratio = trans / smp;
+  this.nmax = this.order / 2;
+  this.nmin = -1 * this.nmax;
+
+  this.set_window();
+  this.set_weights();
+  this.set_taps();
+};
+
+jsfirf.prototype.set_window = function(){
+  "use strict";
+  var n;
+  this.win_weights = [];
+  if(this.win_type == "rectangular"){
+    for(n = 0; n <= this.order; n++)
+      this.win_weights.push(1.0);
+  } else if(this.win_type == "bartlett"){
+    for(n = 0; n <= this.order; n++)
+      this.win_weights.push(1 - (2 * Math.abs(n - this.nmax) / this.order));
+  } else if(this.win_type == "hanning"){
+    for(n = 0; n <= this.order; n++)
+      this.win_weights.push(0.5 - 0.5 * Math.cos(2 * Math.PI * n / this.order));
+  } else if(this.win_type == "hamming"){
+    for(n = 0; n <= this.order; n++)
+      this.win_weights.push(0.54 - 0.46 * Math.cos(2 * Math.PI * n / this.order));
+  } else if(this.win_type == "blackman"){
+    for(n = 0; n <= this.order; n++)
+      this.win_weights.push(0.42 - 0.5 * Math.cos(2 * Math.PI * n / this.order) + 0.08 * Math.cos(4 * Math.PI * n / this.order));
+  }
+};
+
+jsfirf.prototype.set_weights = function(){
+  "use strict";
+  this.weights = [];
+  var x;
+  var norm_freq = this.trans_freq / this.smp_freq;
+  for(x = this.nmin; x <= this.nmax; x++)
+    if(x == 0)
+      this.weights.push(2 * norm_freq);
+    else
+      this.weights.push(Math.sin(2 * Math.PI * norm_freq * x) / (Math.PI * x));
+};
+
+jsfirf.prototype.set_taps = function(){
+  "use strict";
+  var n;
+  this.taps = [];
+  for(n = this.nmin; n <= this.nmax; n++)
+    this.taps.push(this.win_weights[n - this.nmin] * this.weights[n - this.nmin]);
+};
+
+jsfirf.prototype.unity = function(){
+  "use strict";
+  var n;
+  this.taps = [];
+  for(n = this.nmin; n <= this.nmax; n++){
+    if(n == 0)
+      this.taps.push(1.0);
+    else
+      this.taps.push(0);
+  }
+};
+
+jsfirf.prototype.set_val = function(dvw, idx, bytes, val){
+  "use strict";
+  if(bytes == 1)
+    dvw.setInt8(idx * bytes, val);
+  else if(bytes == 2)
+    dvw.setInt16(idx * bytes, val);
+  else if(bytes == 4)
+    dvw.setInt32(idx * bytes, val);
+}
+
+jsfirf.prototype.get_val = function(dvw, idx, bytes){
+  "use strict";
+  var val;
+  if(bytes == 1)
+    val = dvw.getInt8(idx * bytes);
+  else if(bytes == 2)
+    val = dvw.getInt16(idx * bytes);
+  else if(bytes == 4)
+    val = dvw.getInt32(idx * bytes);
+  return val;
+}
+
+jsfirf.prototype.get_array = function(dvw, bytes){
+  "use strict";
+  var val, arr = [];
+  for(var x = 0; x < dvw.byteLength; x += bytes){
+    if(bytes == 1)
+      val = dvw.getInt8(x);
+    else if(bytes == 2)
+      val = dvw.getInt16(x);
+    else if(bytes == 4)
+      val = dvw.getInt32(x);
+    arr.push(val);
+  }
+  return arr;
+}
+
+jsfirf.prototype.apply = function(data, bits){
+  "use strict";
+  var result, tapmax, filtered = new ArrayBuffer(data.byteLength);
+  var ivw = new DataView(data);
+  var ovw = new DataView(filtered);
+  var bytes = bits / 8;
+  var len = data.byteLength / bytes;
+  for(var x = 0; x < len; x += bytes){
+    result = 0;
+    tapmax = this.order;
+    if(x < this.order)
+      result = 0; //this.get_val(ivw, x, bytes);
+    else for(var n = 0; n <= tapmax; n++)
+      result += this.taps[n] * this.get_val(ivw, x - tapmax + n, bytes);
+    this.set_val(ovw, x, bytes, result);
+  }
+  return filtered;
+};
+
+jsfirf.prototype.lopass = function(data, trans, bits){
+  "use strict";
+  var frate = this.smp_freq, forder = this.order, fwin = this.win_type;
+  if(typeof frate == "undefined")
+    return [];
+  if(typeof forder == "undefined")
+    forder = 20;
+  if(typeof fwin == "undefined")
+    fwin = "rectangular";
+  var filter = new jsfirf(frate, trans, forder, fwin);
+  var filtered = filter.apply(data, bits);
+  return filtered;
+};
+
+jsfirf.prototype.hipass = function(data, trans, bits){
+  "use strict";
+  var frate = this.smp_freq, forder = this.order, fwin = this.win_type;
+  if(typeof frate == "undefined")
+    return [];
+  if(typeof forder == "undefined")
+    forder = 20;
+  if(typeof fwin == "undefined")
+    fwin = "rectangular";
+  var lowpass = new jsfirf(frate, trans, forder, fwin);
+  var allpass = new jsfirf(frate, trans, forder, fwin);
+  allpass.unity();
+  var low = lowpass.apply(data, bits);
+  var all = allpass.apply(data, bits);
+  var lowvw = new DataView(low);
+  var allvw = new DataView(all);
+  var high = new ArrayBuffer(data.byteLength);
+  var highvw = new DataView(high);
+  var bytes = bits / 8;
+  var val;
+  for(var x = 0; x < (low.byteLength / bytes); x++){
+    val = this.get_val(allvw, x, bytes) - this.get_val(lowvw, x, bytes);
+    this.set_val(highvw, x, bytes, val);
+  }
+  return high;
+};
+
+jsfirf.prototype.bandpass = function(data, lotrans, hitrans, bits){
+  "use strict";
+  var frate = this.smp_freq, forder = this.order, fwin = this.win_type;
+  if(typeof frate == "undefined")
+    return [];
+  if(typeof forder == "undefined")
+    forder = 20;
+  if(typeof fwin == "undefined")
+    fwin = "rectangular";
+  var hipass = this.hipass(data, lotrans, bits);
+  var bandpass = this.lopass(hipass, hitrans, bits);
+  return bandpass;
+};
---
+++ jsfirf_example.js
@@ -0,0 +1,175 @@
+// License information: MIT License
+//
+// Copyright (c) Chris Schalick 2016 All Rights Reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Tree structure code creates an object hierarchy on demand
+// inside an HTML table, allows click to expand / collapse
+// to view and traverse the hierarchy.
+
+var filtered = {};
+filtered.pts = 10000;
+filtered.zoom_pos = 0.5;
+filtered.zoom_factor = 1;
+
+function g(id){
+  "use strict";
+  return document.getElementById(id);
+}
+
+function show_response(id, bfr, bits){
+  "use strict";
+  var bytes = bits / 8, r = g(id), rctx = r.getContext("2d");
+  var dvw = new DataView(bfr), data = jsfirf.prototype.get_array(dvw, bytes);
+  rctx.fillStyle = "#f0f0f0";
+  rctx.fillRect(0,0,300,100);
+  var x, x1 = 0, x2, y1=50, y2;
+  var dx = r.width / (data.length / filtered.zoom_factor);
+  var pts_shown = r.width / dx;
+  var st;
+  if(filtered.zoom_factor == 1)
+    st = 0;
+  else
+    st = Math.round(filtered.zoom_pos * filtered.pts - pts_shown / 2);
+  if(st < 0)
+    st = 0;
+  rctx.strokeStyle = '#0000ff';
+  for(x = st; x < data.length; x++){
+    x2 = x1 + dx;
+    y2 = 50 + data[x];
+    rctx.beginPath();
+    rctx.moveTo(x1, y1);
+    rctx.lineTo(x2, y2);
+    rctx.stroke();
+    x1 = x2; y1 = y2;
+    if(x1 > r.width)
+      x = data.length;
+  }
+  rctx.strokeStyle = '#00ff00';
+  rctx.beginPath();
+  rctx.moveTo(filtered.zoom_pos * r.width, 0);
+  rctx.lineTo(filtered.zoom_pos * r.width, 100);
+  rctx.stroke();
+}
+
+g("go").onclick = function(){
+  "use strict";
+  filtered.smprate = g("smprate").value * 1.0;
+  filtered.lofreq = g("lopass").value * 1.0;
+  filtered.hifreq = g("hipass").value * 1.0;
+  var p0 = filtered.smprate / (filtered.lofreq / 10);
+  var p1 = filtered.smprate / (filtered.hifreq * 10);
+  if(p1 < 4)
+    p1 = 4;
+  var thisper, x, displ = filtered.pts;
+  filtered.bits = g("bits").value * 1.0;
+  filtered.bytes = filtered.bits / 8;
+  filtered.inp = new ArrayBuffer(displ * filtered.bytes);
+  filtered.chirp = new DataView(filtered.inp);
+  var val, pos, dispf = displ / 100;
+  for(x = 0; x < displ; x++)
+  {
+    pos = x / displ;
+    thisper = (pos * p1) + (1 - pos) * p0;
+    val = Math.round(50 * Math.sin(x / thisper * 2 * Math.PI));
+    if(filtered.bytes == 1)
+      filtered.chirp.setInt8(x, val);
+    else if(filtered.bytes == 2)
+      filtered.chirp.setInt16(x * 2, val);
+    else if(filtered.bytes == 4)
+      filtered.chirp.setInt32(x * 4, val);
+  }
+
+  filtered.order = g("order").value * 1.0;
+  filtered.win = g("fwindow").value;
+  var filter = new jsfirf(filtered.smprate, filtered.lofreq, filtered.order, filtered.win);
+  filtered.lopass = filter.lopass(filtered.inp, filtered.hifreq, filtered.bits);
+  filtered.hipass = filter.hipass(filtered.inp, filtered.lofreq, filtered.bits);
+  filtered.bandpass = filter.bandpass(filtered.inp, filtered.lofreq, filtered.hifreq, filtered.bits);
+  filtered.zoom_pos = 150;
+  filtered.zoom_factor = 1;
+  show_filtered();
+}
+
+function show_filtered(){
+  "use strict";
+  show_response("input_response", filtered.inp, filtered.bits);
+  show_response("lowpass_response", filtered.lopass, filtered.bits);
+  show_response("highpass_response", filtered.hipass, filtered.bits);
+  show_response("bandpass_response", filtered.bandpass, filtered.bits);
+}
+
+g("input_response").oncontextmenu = function(e){
+  "use strict";
+  e.preventDefault();
+};
+
+g("input_response").onmousedown = function(ev){
+  "use strict";
+  var cv = g("input_response"), click_x, min_x, max_x, ctr_x, wid_x;
+  if(ev.x != undefined)
+    click_x = event.x;
+  else 
+    click_x = ev.clientX + document.body.scrollLeft +
+              document.documentElement.scrollLeft;
+  click_x -= cv.getBoundingClientRect().left;
+  
+  filtered.mouse = ev.button;
+  if(ev.button != 0){
+    filtered.zoom_factor = 1;
+    filtered.zoom_pos = 0.5;
+  } else {
+    filtered.zoom_pos = (click_x / cv.width)
+  }
+  show_filtered();
+  return false;
+}
+
+g("input_response").onmouseup = function(ev){
+  "use strict";
+  filtered.mouse = -1;
+}
+
+g("input_response").onmousemove = function(ev){
+  "use strict";
+  var cv = g("input_response"), click_x, min_x, max_x, ctr_x, wid_x;
+  click_x = ev.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+  click_x -= cv.getBoundingClientRect().left;
+  
+  if(filtered.mouse == 0)
+    filtered.zoom_pos = (click_x / cv.width)
+
+  show_filtered();
+  return false;
+}
+
+g("input_response").onwheel = function(ev){
+  "use strict";
+  if(ev.wheelDelta > 0)
+    filtered.zoom_factor++;
+  else
+    filtered.zoom_factor--;
+  if(filtered.zoom_factor < 1)
+    filtered.zoom_factor = 1;
+  if(filtered.zoom_factor > (filtered.pts / 300))
+    filtered.zoom_factor = (filtered.pts / 300);
+  show_filtered();
+  return false;
+}
